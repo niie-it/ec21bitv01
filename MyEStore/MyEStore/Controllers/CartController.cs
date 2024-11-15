269f7c6bec67229b812f08b38d945917aeada072
HIENLTH 2023 <hyhien@gmail.com>
2024-11-15 09:03:12 +0700

    Paypal payment

--- MyEStore/MyEStore/Controllers/CartController.cs
+++ MyEStore/MyEStore/Controllers/CartController.cs
@@ -4,57 +4,63 @@ using MyEStore.Models;
 
 namespace MyEStore.Controllers
 {
-    public class CartController : Controller
-    {
-        private readonly MyeStoreContext _ctx;
+	public class CartController : Controller
+	{
+		private readonly MyeStoreContext _ctx;
+		private readonly PaypalClient _paypalClient;
 
-        public CartController(MyeStoreContext ctx) { _ctx = ctx; }
+		public CartController(MyeStoreContext ctx, PaypalClient paypalClient)
+		{
+			_ctx = ctx;
+			_paypalClient = paypalClient;
+		}
 
-        public const string CART_NAME = "CART";
-        public List<CartItem> CartItems
-        {
-            get
-            {
-                var carts = HttpContext.Session.Get<List<CartItem>>(CART_NAME) ?? new List<CartItem>();
-                return carts;
-            }
-        }
+		public const string CART_NAME = "CART";
+		public List<CartItem> CartItems
+		{
+			get
+			{
+				var carts = HttpContext.Session.Get<List<CartItem>>(CART_NAME) ?? new List<CartItem>();
+				return carts;
+			}
+		}
 
-        public IActionResult Index()
-        {
-            return View(CartItems);
-        }
+		public IActionResult Index()
+		{
+			ViewBag.PaypalClientId = _paypalClient.ClientId;
+			return View(CartItems);
+		}
 
-        public IActionResult AddToCart(int id, int qty = 1)
-        {
-            var cart = CartItems;
-            //kiểm tra xem có ko?
-            var cartItem = cart.SingleOrDefault(p => p.MaHh == id);
-            if (cartItem != null)
-            {
-                cartItem.SoLuong += qty;
-            }
-            else
-            {
-                var hangHoa = _ctx.HangHoas.SingleOrDefault(p => p.MaHh == id);
-                if (hangHoa == null)
-                {
-                    TempData["ThongBao"] = $"Tìm không thấy sản phẩm có mã {id}";
-                    return RedirectToAction("Index", "Product");
-                }
-                cartItem = new CartItem
-                {
-                    MaHh = id,
-                    SoLuong = qty,
-                    TenHh = hangHoa.TenHh,
-                    DonGia = hangHoa.DonGia ?? 0,
-                    Hinh = hangHoa.Hinh
-                };
-                cart.Add(cartItem);
-            }
-            //set session
-            HttpContext.Session.Set<List<CartItem>>(CART_NAME, cart);
-            return RedirectToAction("Index");
-        }
-    }
+		public IActionResult AddToCart(int id, int qty = 1)
+		{
+			var cart = CartItems;
+			//kiểm tra xem có ko?
+			var cartItem = cart.SingleOrDefault(p => p.MaHh == id);
+			if (cartItem != null)
+			{
+				cartItem.SoLuong += qty;
+			}
+			else
+			{
+				var hangHoa = _ctx.HangHoas.SingleOrDefault(p => p.MaHh == id);
+				if (hangHoa == null)
+				{
+					TempData["ThongBao"] = $"Tìm không thấy sản phẩm có mã {id}";
+					return RedirectToAction("Index", "Product");
+				}
+				cartItem = new CartItem
+				{
+					MaHh = id,
+					SoLuong = qty,
+					TenHh = hangHoa.TenHh,
+					DonGia = hangHoa.DonGia ?? 0,
+					Hinh = hangHoa.Hinh
+				};
+				cart.Add(cartItem);
+			}
+			//set session
+			HttpContext.Session.Set<List<CartItem>>(CART_NAME, cart);
+			return RedirectToAction("Index");
+		}
+	}
 }
---
+++ MyEStore/MyEStore/Controllers/PaymentController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MyEStore.Models;
+
+namespace MyEStore.Controllers
+{
+	public class PaymentController : Controller
+	{
+		private readonly PaypalClient _paypalClient;
+
+		public PaymentController(PaypalClient paypalClient)
+		{
+			_paypalClient = paypalClient;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public const string CART_NAME = "CART";
+		public List<CartItem> CartItems
+		{
+			get
+			{
+				var carts = HttpContext.Session.Get<List<CartItem>>(CART_NAME) ?? new List<CartItem>();
+				return carts;
+			}
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> PaypalOrder(CancellationToken cancellationToken)
+		{
+			// Tạo đơn hàng (thông tin lấy từ Session???)
+			//var tongTien = "1009.0";
+			var tongTien = CartItems.Sum(p => p.ThanhTien).ToString();
+			var donViTienTe = "USD";
+			// OrderId - mã tham chiếu (duy nhất)
+			var orderIdref = "DH" + DateTime.Now.Ticks.ToString();
+
+			try
+			{
+				// a. Create paypal order
+				var response = await _paypalClient.CreateOrder(tongTien, donViTienTe, orderIdref);
+
+				return Ok(response);
+			}
+			catch (Exception e)
+			{
+				var error = new
+				{
+					e.GetBaseException().Message
+				};
+
+				return BadRequest(error);
+			}
+		}
+
+		public async Task<IActionResult> PaypalCapture(string orderId, CancellationToken cancellationToken)
+		{
+			try
+			{
+				var response = await _paypalClient.CaptureOrder(orderId);
+
+				var reference = response.purchase_units[0].reference_id;
+
+				// Put your logic to save the transaction here
+				// You can use the "reference" variable as a transaction key
+				// Lưu đơn hàng vô database
+
+				return Ok(response);
+			}
+			catch (Exception e)
+			{
+				var error = new
+				{
+					e.GetBaseException().Message
+				};
+
+				return BadRequest(error);
+			}
+		}
+
+		[Authorize]
+		public IActionResult Success()
+		{
+			return View();
+		}
+	}
+}
